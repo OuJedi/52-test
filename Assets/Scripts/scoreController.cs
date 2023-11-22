165bf768701c998c46f0edd2b982642981622b5a
Kamal Zitouni <zitounikamal@hotmail.fr>
2023-11-22 05:03:26 +0100

    change scoreController class name

--- Assets/Scripts/scoreController.cs
+++ Assets/Scripts/scoreController.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-public class scoreController : MonoBehaviour
+public class ScoreController : MonoBehaviour
 {
     
     private TextMeshProUGUI scoreText;
